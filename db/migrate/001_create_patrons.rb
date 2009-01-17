4c906d486fd7198a9c4e4cb476d72dad6eb74fb6
Kosuke Tanabe <tanabe@kamata.lib.teu.ac.jp>
2009-01-18 02:24:08 +0900

    added telephone_number to Patron

--- db/migrate/001_create_patrons.rb
+++ db/migrate/001_create_patrons.rb
@@ -1,42 +1,44 @@
 class CreatePatrons < ActiveRecord::Migration
   def self.up
     create_table :patrons do |t|
-      t.column :parent_id, :integer
-      t.column :last_name, :string
-      t.column :middle_name, :string
-      t.column :first_name, :string
-      t.column :last_name_transcription, :string
-      t.column :middle_name_transcription, :string
-      t.column :first_name_transcription, :string
-      t.column :corporate_name, :string
-      t.column :corporate_name_transcription, :string
-      t.column :full_name, :string, :null => false
-      t.column :full_name_transcription, :text
-      t.column :full_name_alternative, :text
-      t.column :created_at, :datetime
-      t.column :updated_at, :datetime
-      t.column :deleted_at, :datetime
-      t.column :zip_code_1, :string
-      t.column :zip_code_2, :string
-      t.column :address_1, :text
-      t.column :address_2, :text
-      t.column :address_1_note, :text
-      t.column :address_2_note, :text
-      t.column :other_designation, :text
-      t.column :place, :text
-      t.column :date_of_birth, :timestamp
-      t.column :date_of_death, :timestamp
-      t.column :language_id, :integer, :default => 1, :null => false
-      t.column :country_id, :integer, :default => 1, :null => false
-      t.column :patron_type_id, :integer, :default => 1, :null => false
-      t.column :lock_version, :integer, :default => 0, :null => false
-      t.column :note, :text
-      t.column :creates_count, :integer, :default => 0, :null => false
-      t.column :realizes_count, :integer, :default => 0, :null => false
-      t.column :produces_count, :integer, :default => 0, :null => false
-      t.column :owns_count, :integer, :default => 0, :null => false
-      t.column :resource_has_subjects_count, :integer, :default => 0, :null => false
-      t.column :access_role_id, :integer, :null => false
+      t.integer :parent_id
+      t.string :last_name
+      t.string :middle_name
+      t.string :first_name
+      t.string :last_name_transcription
+      t.string :middle_name_transcription
+      t.string :first_name_transcription
+      t.string :corporate_name
+      t.string :corporate_name_transcription
+      t.string :full_name, :null => false
+      t.text :full_name_transcription
+      t.text :full_name_alternative
+      t.timestamps
+      t.datetime :deleted_at
+      t.string :zip_code_1
+      t.string :zip_code_2
+      t.text :address_1
+      t.text :address_2
+      t.text :address_1_note
+      t.text :address_2_note
+      t.string :telephone_number_1
+      t.string :telephone_number_2
+      t.string :fax_number
+      t.text :other_designation
+      t.text :place
+      t.datetime :date_of_birth
+      t.datetime :date_of_death
+      t.integer :language_id, :default => 1, :null => false
+      t.integer :country_id, :default => 1, :null => false
+      t.integer :patron_type_id, :default => 1, :null => false
+      t.integer :lock_version, :default => 0, :null => false
+      t.text :note
+      t.integer :creates_count, :default => 0, :null => false
+      t.integer :realizes_count, :default => 0, :null => false
+      t.integer :produces_count, :default => 0, :null => false
+      t.integer :owns_count, :default => 0, :null => false
+      t.integer :resource_has_subjects_count, :default => 0, :null => false
+      t.integer :access_role_id, :null => false
       t.string :state
     end
     add_index :patrons, :parent_id
