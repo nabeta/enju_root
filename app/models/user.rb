b05c6c172fe2b60d75862b5511b57c778fb38ca5
Kosuke Tanabe <tanabe@enju.slis.keio.ac.jp>
2009-03-11 04:29:50 +0900

    trivial fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,6 @@ class User < ActiveRecord::Base
   attr_reader :auto_generated_password
   attr_accessor :first_name, :middle_name, :last_name, :full_name, :first_name_transcription, :middle_name_transcription, :last_name_transcription, :full_name_transcription
   attr_accessor :zip_code, :address, :telephone_number, :fax_number, :address_note
-  attr_encrypted :note_add, :key => 'hogehoge'
 
   validates_presence_of     :login, :user_number, :full_name
   validates_length_of       :login,    :within => 3..40
