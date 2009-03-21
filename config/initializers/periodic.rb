b878a5fff62f1f16414c84e02a0be8ca8d48e1bb
Kosuke Tanabe <tanabe@kamata.lib.teu.ac.jp>
2009-03-21 09:46:33 +0900

    misspelled

--- config/initializers/periodic.rb
+++ config/initializers/periodic.rb
@@ -1,11 +1,11 @@
 scheduler = Rufus::Scheduler.start_new
 
 scheduler.cron "0 4 * * *" do
- user_checkout_stat = UserCheckoutStat.create(:from_date => from_date, :to_date => to_date)
+  user_checkout_stat = UserCheckoutStat.create(:from_date => from_date, :to_date => to_date)
   user_checkout_stat.culculate_users_count
   manifestation_checkout_stat = ManifestationCheckoutStat.create(:from_date => from_date, :to_date => to_date)
   manifestation_checkout_stat.culculate_manifestations_count
- user_reserve_stat = UserReserveStat.create(:from_date => from_date, :to_date => to_date)
+  user_reserve_stat = UserReserveStat.create(:from_date => from_date, :to_date => to_date)
   user_reserve_stat.culculate_users_count
   manifestation_reserve_stat = ManifestationReserveStat.create(:from_date => from_date, :to_date => to_date)
   manifestation_reserve_stat.culculate_manifestations_count
