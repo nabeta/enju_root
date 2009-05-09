40b6bfae4ac527f5df05e8ca50d8c6da737f6b1c
Kosuke Tanabe <nabeta@x-il.jp>
2009-05-09 14:58:37 +0900

    ALTER TABLE tables ALTER COLUMN name TYPE text;

---
+++ db/migrate/20090509053327_copy_name_to_display_name.rb
@@ -0,0 +1,13 @@
+class CopyNameToDisplayName < ActiveRecord::Migration
+  def self.up
+    models = %w(checkout_types circulation_statuses classification_types countries event_categories expression_forms frequency_of_issues languages libraries library_groups manifestation_forms patron_types request_status_types request_types shelves subject_heading_types subject_types use_restrictions user_groups work_forms)
+    models.each do |model|
+      sql = "UPDATE #{model} SET display_name = name"
+      execute(sql)
+      change_column(model, :name, :text)
+    end
+  end
+
+  def self.down
+  end
+end
