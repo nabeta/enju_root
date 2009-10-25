1425bae0cacc9c082c0e1c65931b6671ff6cdfc4
Kosuke Tanabe <nabeta@x-il.jp>
2009-10-25 16:36:42 +0900

    library_group name cache was not refreshed

--- app/models/library_group_sweeper.rb
+++ app/models/library_group_sweeper.rb
@@ -1,7 +1,9 @@
 class LibraryGroupSweeper < ActionController::Caching::Sweeper
   observe LibraryGroup
   def after_save(record)
-    expire_fragment(:controller => 'library_groups', :id => record.id, :action_suffix => 'header')
+    I18n.available_locales.each do |locale|
+      expire_fragment(:controller => 'library_groups', :id => record.id, :action_suffix => 'header', :locale => locale)
+    end
   end
 
   def after_destroy(record)
