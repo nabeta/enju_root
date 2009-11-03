fb8bdbdd887295534b0e5e58ba8c29554350ea6f
Kosuke Tanabe <nabeta@x-il.jp>
2009-11-03 14:27:15 +0900

    workaround for sending mail with TLS

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,8 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "Binary Logic Notifier <noreply@library.example.jp>"
+    #from          "Binary Logic Notifier <noreply@library.example.jp>"
+    from          "noreply@library.example.jp"
     recipients    user.email
     sent_on       Time.zone.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
@@ -11,7 +12,8 @@ class Notifier < ActionMailer::Base
 
   def message_notification(user)
     subject       I18n.t('message.new_message_from_library', :library => LibraryGroup.site_config.display_name.localize)
-    from          "#{LibraryGroup.site_config.display_name.localize} <#{LibraryGroup.site_config.email}>"
+    #from          "#{LibraryGroup.site_config.display_name.localize} <#{LibraryGroup.site_config.email}>"
+    from          "#{LibraryGroup.site_config.email}"
     recipients    user.email
     sent_on       Time.zone.now
     body          :user => user
@@ -19,7 +21,8 @@ class Notifier < ActionMailer::Base
 
   def manifestation_info(user, manifestation)
     subject       "#{manifestation.original_title} : #{LibraryGroup.site_config.display_name.localize}"
-    from          "#{LibraryGroup.site_config.display_name.localize} <#{LibraryGroup.site_config.email}>"
+    #from          "#{LibraryGroup.site_config.display_name.localize} <#{LibraryGroup.site_config.email}>"
+    from          "#{LibraryGroup.site_config.email}"
     recipients    user.email
     sent_on       Time.zone.now
     body          :user => user, :manifestation => manifestation
