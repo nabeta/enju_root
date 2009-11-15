d274b9f46d04f22812710d04c3d052e98d704819
Kosuke Tanabe <nabeta@x-il.jp>
2009-11-15 17:34:52 +0900

    added tests

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -7,4 +7,10 @@ class EventTest < ActiveSupport::TestCase
     events(:event_00001).all_day = true
     assert_equal events(:event_00001).set_all_day, events(:event_00001).end_at.end_of_day
   end
+
+  def test_set_all_day_beginning_of_day
+    events(:event_00008).all_day = true
+    end_at = events(:event_00008).end_at
+    assert_equal events(:event_00008).set_all_day, (end_at - 1.day).end_of_day
+  end
 end
