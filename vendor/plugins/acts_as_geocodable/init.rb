401367c7debf1c0dd702da1542838aa498965ce6
Kosuke Tanabe <nabeta@x-il.jp>
2009-12-25 18:46:37 +0900

    acts_as_geocodable gem

--- vendor/plugins/acts_as_geocodable/init.rb
+++
@@ -1,9 +0,0 @@
-begin 
-  require 'graticule'
-rescue LoadError
-  raise "Graticule >= 0.2.0 is required for acts_as_geocodable"
-end
-require 'acts_as_geocodable'
-
-ActiveRecord::Base.send :include, CollectiveIdea::Acts::Geocodable
-ActionController::Base.send :include, CollectiveIdea::RemoteLocation
