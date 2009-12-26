c8899eadc3a3b25ec3f617ed8240ccbe0413b182
Kosuke Tanabe <nabeta@x-il.jp>
2009-12-26 23:10:31 +0900

    shoud not create duplicate taggings (acts-as-taggable-on 1.0.16)

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,5 +2,9 @@ class Tagging < ActiveRecord::Base #:nodoc:
   belongs_to :tag, :counter_cache => true
   belongs_to :taggable, :polymorphic => true
   belongs_to :tagger, :polymorphic => true
+  
   validates_presence_of :context
+  validates_presence_of :tag_id
+  
+  validates_uniqueness_of :tag_id, :scope => [:taggable_type, :taggable_id, :context]
 end
