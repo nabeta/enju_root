e7e7df3547bb6469993318a5f058a87febcb17c8
Kosuke Tanabe <nabeta@x-il.jp>
2010-02-21 19:07:52 +0900

    merged master

--- app/models/user.rb
+++ app/models/user.rb
@@ -93,8 +93,8 @@ class User < ActiveRecord::Base
   #validates_length_of       :login,    :within => 2..40
   #validates_uniqueness_of   :login,    :case_sensitive => false
 
-  #validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100, :if => Proc.new{|user| !user.email.blank?}, :allow_nil => true
+  validates_presence_of     :email, :on => :create, :if => proc{|user| !user.operator.try(:has_role?, 'Librarian')}
+  validates_length_of       :email,    :within => 6..100, :if => proc{|user| !user.email.blank?}, :allow_nil => true
   validates_uniqueness_of   :email, :case_sensitive => false, :if => proc{|user| !user.email.blank?}, :allow_nil => true
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :allow_blank => true
   validates_associated :patron, :user_group, :library
@@ -103,6 +103,7 @@ class User < ActiveRecord::Base
   #validates_uniqueness_of :user_number, :with=>/\A[0-9]+\Z/, :allow_blank => true
   validates_uniqueness_of :user_number, :with=>/\A[0-9A-Za-z_]+\Z/, :allow_blank => true
   validate_on_update :verify_password
+  #validates_acceptance_of :confirmed
 
   def verify_password
     errors.add(:old_password) if self.password_not_verified
@@ -230,7 +231,8 @@ class User < ActiveRecord::Base
 
   def activate!
     activate
-    self.patron = Patron.create(:full_name => self.login) unless self.patron
+    # TODO: パトロン作成のタイミングを決める
+    #self.patron = Patron.create(:full_name => self.login) unless self.patron
     save
   end
 
