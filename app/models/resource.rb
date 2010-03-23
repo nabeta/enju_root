eb7b66ca7a6798e48a19bc961ff1c69fdb16a9e7
Kosuke Tanabe <nabeta@x-il.jp>
2010-03-23 23:19:04 +0900

    disabled paper_trail in user model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -29,7 +29,8 @@ class Resource < ActiveRecord::Base
   aasm_state :approved
   aasm_state :rejected
 
-  aasm_initial_state :pending
+  #aasm_initial_state :pending
+  aasm_initial_state :not_approved
 
   aasm_event :aasm_ask_for_approval do
     transitions :from => [:pending, :published, :approved, :not_approved, :rejected],
@@ -43,35 +44,26 @@ class Resource < ActiveRecord::Base
 
   aasm_event :aasm_publish do
     transitions :from => [:approved],
-      :to => :published,
-      :on_transition => :clear_approval
+      :to => :published
   end
   #TODO: 却下処理
+
+  attr_accessor :approve, :publish
 
   def per_page
     10
   end
 
   def before_save
-    if state == 'approved' and approved
+    if ['approved', 'published'].include?(state) and publish == '1'
       aasm_publish
       return
     end
-    if approved_change
-      if approved_change[0] == false
-        aasm_approve
-      end
+    if state == 'not_approved' and approve == '1'
+      aasm_approve
     else
       aasm_ask_for_approval
     end
-  end
-
-  def ask_for_approval
-    aasm_ask_for_approval
-  end
-
-  def clear_approval
-    self.approved = false
   end
 
   def dcndl
