0772f3f8f576c179f21ffb880a869be9af1da6d3
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2010-04-05 17:51:40 +0900

    require delayed_job 2.0.1

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,4 @@
-Delayed::Job.destroy_failed_jobs = false
-silence_warnings do
-  Delayed::Job.const_set("MAX_ATTEMPTS", 3)
-  Delayed::Job.const_set("MAX_RUN_TIME", 5.minutes)
-end 
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 5.minutes
