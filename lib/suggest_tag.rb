3908bf2598b996a07cc8f0fd21467bc51e23ec5e
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2010-04-07 07:43:03 +0900

    tag suggestion didn't work

--- lib/suggest_tag.rb
+++ lib/suggest_tag.rb
@@ -2,13 +2,11 @@ module SuggestTag
   def suggest_tags
     tags = []
     threshold = (self.strip.split(//).size * 0.2).round
-    Tag.find(:all, :order => 'taggings_count DESC', :limit => 100).each do |t|
+    Bookmark.tag_counts(:limit => 100).each do |t|
       distance = Text::Levenshtein.distance(t.name, self)
       tags << t if distance <= threshold
     end
     tags
-  rescue
-    []
   end
 end
 
