61bc668f0b764ae8b6362d85539573b39ddb19e1
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2010-05-27 13:06:11 +0900

    require will_paginate 2.3.14 and latest rails_xss

--- vendor/plugins/rails_xss/init.rb
+++ vendor/plugins/rails_xss/init.rb
@@ -1,16 +1,7 @@
-# Include hook code here
-begin
-  require 'erubis/helpers/rails_helper'
-  require 'rails_xss'
-
-  Erubis::Helpers::RailsHelper.engine_class = RailsXss::Erubis
-
-  Module.class_eval do
-    include RailsXss::SafeHelpers
+unless $gems_rake_task
+  if Rails.version <= "2.3.7"
+    $stderr.puts "rails_xss requires Rails 2.3.8 or later. Please upgrade to enable automatic HTML safety."
+  else
+    require 'rails_xss'
   end
-
-  require 'rails_xss_escaping'
-  require 'av_patch'
-rescue LoadError
-  puts "Could not load all modules required by rails_xss. Please make sure erubis is installed an try again."
-end unless $gems_rake_task
+end
