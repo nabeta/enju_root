a8d72ce48776e559205a7ee31f89f8de73ccec38
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-11-30 03:06:42 +0900

    updated initialize files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,8 +4,8 @@
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-#EnjuRoot::Application.config.session_store :active_record_store
+# (create the session table with "rails generate session_migration")
+# EnjuRoot::Application.config.session_store :active_record_store
 
 require 'action_dispatch/middleware/session/dalli_store'
 EnjuRoot::Application.config.session_store :dalli_store, :key => '_enju_root_session'
