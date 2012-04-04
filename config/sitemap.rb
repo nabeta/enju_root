021be587f50212f588a8dfa3e49cc8a4f8c55de1
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-04-04 14:25:41 +0900

    updated template files

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,37 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = URI.parse("http://#{LIBRARY_WEB_HOSTNAME}:#{LIBRARY_WEB_PORT_NUMBER}").normalize.to_s
 
-SitemapGenerator::Sitemap.add_links do |sitemap|
+SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
   #
-  # The root path '/' and sitemap index file are added automatically.
+  # The root path '/' and sitemap index file are added automatically for you.
   # Links are added to the Sitemap in the order they are specified.
   #
-  # Usage: sitemap.add path, options
+  # Usage: add(path, options={})
   #        (default options are used if you don't specify)
   #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly', 
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
   #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
 
-  
-  sitemap.add manifestations_path, :priority => 0.7, :changefreq => 'daily'
+  add manifestations_path, :priority => 0.7, :changefreq => 'daily'
 
   Manifestation.find_each do |manifestation|
-    sitemap.add manifestation_path(manifestation), :lastmod => manifestation.updated_at
+    add manifestation_path(manifestation), :lastmod => manifestation.updated_at
   end
 
   Patron.find_each do |patron|
-    sitemap.add patron_path(patron), :lastmod => patron.updated_at
+    add patron_path(patron), :lastmod => patron.updated_at
   end
-
 end
